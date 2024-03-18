f5a8cdd9326aa836f1969cbdc37c38f916df2ad1
donny <doniwahyu14@gmail.com>
2024-03-19 00:23:39 +0700

    make table component

--- app/components/sidebar/sidebar.tsx
+++ app/components/sidebar/sidebar.tsx
@@ -1,21 +1,35 @@
-import { VStack, Text, Button, Icon, Spacer, Box } from "@chakra-ui/react";
+import {
+  VStack,
+  Text,
+  Button,
+  Icon,
+  Spacer,
+  Box,
+  Divider,
+  HStack,
+} from "@chakra-ui/react";
 import { HiOutlineNewspaper } from "react-icons/hi2";
 import { VscGraph } from "react-icons/vsc";
 import { GrMoney } from "react-icons/gr";
 import { PiSignOutLight } from "react-icons/pi";
 import { GoHome } from "react-icons/go";
+import { BsArrowBarLeft } from "react-icons/bs";
+import { LuArrowLeftFromLine } from "react-icons/lu";
 
 export default function Sidebar() {
   return (
     <>
       <VStack
         p="0"
-        border="1px"
-        borderColor="gray.200"
+        // border="1px"
+        // borderColor="gray.200"
         py="5"
         w={[null, null, "30vw", "15vw"]}
         h="100vh"
-        bgColor={"#F6F7F9"}
+        // boxShadow={"xl"}
+        // bgColor={"#fc9356"}
+        bgColor={"#ffede3"}
+        // bgGradient="linear(to-b, #fc9356, #ffede3)"
       >
         <VStack
           pb="5"
@@ -24,31 +38,49 @@ export default function Sidebar() {
           spacing="3"
           // color={"#F6F7F9"}
         >
-          <Text
-            w={"90%"}
-            fontSize="2xl"
-            fontWeight="bold"
-            textAlign={"left"}
-            pl="4"
-            pb="5"
-            borderBottom={"1px"}
-            borderColor={"gray.400"}
-            color="gray.700"
-          >
-            eReport
-          </Text>
+          <HStack w="100%" pb="5" px="5">
+            <Text
+              w={"90%"}
+              fontSize="2xl"
+              fontWeight="bold"
+              textAlign={"left"}
+              color="gray.700"
+            >
+              eReport
+            </Text>
+            <Spacer />
+            <Text
+              fontSize={"20"}
+              // border={"1px solid black"}
+              py={"3px"}
+              pl={"3px"}
+              mr={"0px"}
+              _hover={{ cursor: "pointer", color: "#FE5E37" }}
+            >
+              <LuArrowLeftFromLine />
+            </Text>
+          </HStack>
+          <Divider
+            color={"gray.400"}
+            bgColor={"gray.400"}
+            // border={"1px"}
+          />
           <Button
             w="90%"
             mt="6"
             variant="ghost"
             color="gray.700"
             justifyContent="left"
-            _hover={{ color: "#FE5E37", bgColor: "#ECEEF0" }}
+            _hover={{ color: "#FE5E37", bgColor: "white" }}
+            _active={{ color: "#FE5E37", bgColor: "white" }}
             size="lg"
+            // isActive
+            leftIcon={
+              <Text fontSize={"x-large"}>
+                <GoHome />
+              </Text>
+            }
           >
-            <Text fontSize={"x-large"}>
-              <GoHome />
-            </Text>
             <Text ps="2" fontSize={"18"}>
               Dashboard
             </Text>
@@ -58,54 +90,72 @@ export default function Sidebar() {
             variant="ghost"
             color="gray.700"
             justifyContent="left"
-            _hover={{ color: "#FE5E37", bgColor: "#ECEEF0" }}
+            _hover={{ color: "#FE5E37", bgColor: "white" }}
+            _active={{ color: "#FE5E37", bgColor: "white" }}
             size="lg"
+            isActive
+            leftIcon={
+              <Text fontSize={"x-large"}>
+                <HiOutlineNewspaper />
+              </Text>
+            }
           >
-            <Text fontSize={"x-large"}>
-              <HiOutlineNewspaper />
-            </Text>
             <Text ps="2" fontSize={"18"}>
               Production
             </Text>
           </Button>
+
           <Button
             w="90%"
             variant="ghost"
             color="gray.700"
             justifyContent="left"
-            _hover={{ color: "#FE5E37", bgColor: "#ECEEF0" }}
+            _hover={{ color: "#FE5E37", bgColor: "white" }}
+            _active={{ color: "#FE5E37", bgColor: "white" }}
             size="lg"
+            leftIcon={
+              <Text fontSize={"x-large"}>
+                <VscGraph />
+              </Text>
+            }
           >
-            <Text fontSize={"x-large"}>
-              <VscGraph />
-            </Text>
             <Text ps="2" fontSize={"18"}>
               Surplus
             </Text>
           </Button>
+
           <Button
             w="90%"
             variant="ghost"
             color="gray.700"
             justifyContent="left"
-            _hover={{ color: "#FE5E37", bgColor: "#ECEEF0" }}
+            _hover={{ color: "#FE5E37", bgColor: "white" }}
+            _active={{ color: "#FE5E37", bgColor: "white" }}
             size="lg"
+            leftIcon={
+              <Text fontSize={"x-large"}>
+                <GrMoney />
+              </Text>
+            }
           >
-            <Text fontSize={"x-large"}>
-              <GrMoney />
-            </Text>
             <Text ps="2" fontSize={"18"}>
               Claim
             </Text>
           </Button>
         </VStack>
         <Spacer />
+        <Divider
+          color={"gray.400"}
+          bgColor={"gray.400"}
+          // border={"1px"}
+        />
         <Button
           w="80%"
           mt="5"
           variant="ghost"
           colorScheme="red"
           justifyContent="left"
+          size={"lg"}
         >
           <Text fontSize={"x-large"}>
             <PiSignOutLight />
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,332 @@
-import { VStack, Text, Button, Icon, Spacer } from "@chakra-ui/react";
+import {
+  VStack,
+  Text,
+  Button,
+  Icon,
+  Spacer,
+  HStack,
+  Flex,
+  Input,
+  Box,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
 import { HiOutlineNewspaper } from "react-icons/hi2";
 import { VscGraph } from "react-icons/vsc";
 import { GrMoney } from "react-icons/gr";
 import { PiSignOutLight } from "react-icons/pi";
+import { FaRegFileExcel } from "react-icons/fa";
 import Sidebar from "../components/sidebar/sidebar";
+import { MdOutlineSimCardDownload } from "react-icons/md";
+import { LuArrowLeftFromLine } from "react-icons/lu";
 
 export default function Dashboard() {
   return (
     <>
-      <Sidebar />
+      <Flex>
+        <Sidebar />
+        <VStack w="100%">
+          {/* navbar */}
+          <HStack h={"70px"} w={"100%"} p="20px">
+            {/* <Input
+              flex={0.5}
+              size={"md"}
+              placeholder="Cari no polis"
+              // borderColor={"gray.400"}
+              variant="filled"
+            /> */}
+            <Text fontSize={"20"} fontWeight={"semibold"}>
+              Production Longterm
+            </Text>
+            <Spacer />
+            <Text
+              // flex={1}
+              color={"gray.700"}
+              fontWeight={"normal"}
+              fontSize={"18"}
+            >
+              Welcome, Donny Wahyu
+            </Text>
+          </HStack>
+          {/* main */}
+          <VStack
+            spacing={"10px"}
+            bgColor="#F6F7F9"
+            h={"100%"}
+            w="100%"
+            px="20px"
+            py="10px"
+          >
+            {/* filter */}
+            <Box
+              bgColor="white"
+              borderRadius={"md"}
+              border="1px"
+              borderColor="gray.100"
+              w={"inherit"}
+              h={"100px"}
+              p="20px"
+            >
+              <Text fontSize={"20"} fontWeight={"semibold"}>
+                Filter
+              </Text>
+            </Box>
+
+            {/* table */}
+            <Box
+              bgColor="white"
+              borderRadius={"md"}
+              border="1px"
+              borderColor="gray.100"
+              pt={"10px"}
+              w={"inherit"}
+              maxW={"83vw"}
+              // overflowX={"auto"}
+            >
+              {/* <Box h={"100px"} bgColor={"white"}></Box> */}
+
+              <HStack mx={"20px"} mb="10px">
+                {/* <Text fontSize={"20"} fontWeight={"semibold"}>
+                  Production Longterm
+                </Text> */}
+                <Spacer />
+                <Button variant={"ghost"} colorScheme="teal" size={"md"}>
+                  Download excel
+                  <Text fontSize={"x-large"}>
+                    <FaRegFileExcel />
+                  </Text>
+                  {/* <MdOutlineSimCardDownload /> */}
+                </Button>
+              </HStack>
+              <TableContainer
+                borderBottomRadius={"md"}
+                overflowY={"auto"}
+                overflowX={"auto"}
+                maxH={"780px"}
+              >
+                <Table variant="simple">
+                  <Thead
+                    bgColor={"#fcb890"}
+                    position="sticky"
+                    top="0"
+                    zIndex="sticky"
+                  >
+                    <Tr>
+                      <Th>To convert</Th>
+                      <Th>into</Th>
+                      <Th isNumeric>multiply by</Th>
+                      <Th>into</Th>
+                      <Th>into</Th>
+                      <Th>into</Th>
+                      <Th isNumeric>multiply by</Th>
+                      <Th isNumeric>multiply by</Th>
+                      <Th isNumeric>multiply by</Th>
+                      <Th isNumeric>multiply by</Th>
+                      <Th isNumeric>multiply by</Th>
+                      <Th isNumeric>multiply by</Th>
+                      <Th isNumeric>multiply by</Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    <Tr>
+                      <Td>inches</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+
+                      <Td isNumeric>25.4</Td>
+                    </Tr>
+                    <Tr>
+                      <Td>inches</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                    </Tr>
+                    <Tr>
+                      <Td>inches</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                    </Tr>
+                    <Tr>
+                      <Td>inches</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                    </Tr>
+                    <Tr>
+                      <Td>inches</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                    </Tr>
+                    <Tr>
+                      <Td>inches</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                    </Tr>
+                    <Tr>
+                      <Td>inches</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                    </Tr>
+                    <Tr>
+                      <Td>inches</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                    </Tr>
+                    <Tr>
+                      <Td>inches</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                    </Tr>
+                    <Tr>
+                      <Td>inches</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                    </Tr>
+                    <Tr>
+                      <Td>inches</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                    </Tr>
+                    <Tr>
+                      <Td>inches</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                    </Tr>
+                    <Tr>
+                      <Td>inches</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                    </Tr>
+                    <Tr>
+                      <Td>inches</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                    </Tr>
+                    <Tr>
+                      <Td>inches</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td>millimetres (mm)</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                      <Td isNumeric>25.4</Td>
+                    </Tr>
+                  </Tbody>
+                </Table>
+              </TableContainer>
+            </Box>
+          </VStack>
+        </VStack>
+      </Flex>
     </>
   );
 }
