cb28b9642c638c1f2979a45445eea628db44b7d9
donny <doniwahyu14@gmail.com>
2024-03-28 01:26:58 +0700

    add skeleton when fetching data

--- app/components/mainProdLt/productionLt.tsx
+++ app/components/mainProdLt/productionLt.tsx
@@ -24,6 +24,7 @@ import {
   TableContainer,
   IconButton,
   Select,
+  Skeleton,
 } from "@chakra-ui/react";
 import Navbar from "../navbar/navbar";
 import TableComp from "../table/table";
@@ -35,8 +36,15 @@ type data = {
 
 export default function ProductionLt(data: data) {
   const prodData = useSelector(selectProdLt);
+  const isProdDataEmpty = Object.keys(prodData).length === 0;
+
   const { current_page, max_page, page_size } = prodData.data;
   const dataProd = prodData.data.data;
+  // if (!isProdDataEmpty) {
+  // } else {
+  //   const { current_page, max_page, page_size } =
+  //   const dataProd = prodData.data.data;
+  // }
 
   // console.log(prodData.data);
   // console.log(current_page, max_page, page_size, dataProd);
@@ -65,14 +73,35 @@ export default function ProductionLt(data: data) {
           {/* filter */}
           <Filter isFilter={isFilter} toggleFilter={toggleFilter} />
           {/* table */}
-          <TableComp
-            isOpen={isOpen}
-            isFilter={isFilter}
-            dataProd={dataProd}
-            currentPage={current_page}
-            maxPage={max_page}
-            pageSize={page_size}
-          />
+
+          {isProdDataEmpty || prodData.loading ? (
+            <>
+              <Skeleton
+                mt="10px"
+                // flex="1"
+                bgColor="white"
+                borderRadius={"md"}
+                pt={"10px"}
+                w={"100%"}
+                height={
+                  isFilter
+                    ? [null, null, "60vh", "50vh", "64vh"]
+                    : [null, null, "60vh", "50vh", "78vh"]
+                }
+              />
+            </>
+          ) : (
+            <>
+              <TableComp
+                isOpen={isOpen}
+                isFilter={isFilter}
+                dataProd={dataProd}
+                currentPage={current_page}
+                maxPage={max_page}
+                pageSize={page_size}
+              />
+            </>
+          )}
         </Flex>
       </Flex>
     </>
